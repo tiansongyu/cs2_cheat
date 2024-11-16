ee2e026d82de55417bcf76afdabb1492fd1b7e91
invoker__qq <478880214@qq.com>
2024-11-16 19:08:08 +0800

    chore: Update readme.md
    Signed-off-by: invoker__qq <478880214@qq.com>

--- external-cheat-base/main.cpp
+++ external-cheat-base/main.cpp
@@ -2,6 +2,9 @@
 #include "esp.hpp"
 #include <thread>
 #include <iostream>
+#include <locale>
+#include <codecvt>
+#include <Windows.h>
 
 uint32_t WIDTH;
 uint32_t HEIGHT;
@@ -15,12 +18,18 @@ void autoTriggerThread() {
         if (aimBotEnabled) {
             esp::auto_trigger();
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(1)); // 控制执行频率
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); 
     }
 }
 
 int main()
 {
+    // 设置控制台输出编码为 UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+
+    // 设置区域设置为用户默认区域设置
+    std::locale::global(std::locale(""));
+
     esp::pID = memory::GetProcID(L"cs2.exe");
     esp::modBase = memory::GetModuleBaseAddress(esp::pID, L"client.dll");
     int width = GetSystemMetrics(SM_CXSCREEN);
@@ -76,20 +85,6 @@ int main()
         {
             esp::aim_bot();
         }
-
-        // if (GetAsyncKeyState(VK_RCONTROL) & 0x8000)
-        // {
-        //     esp::auto_aimBotEnabled = !esp::auto_aimBotEnabled;
-        //     if (esp::auto_aimBotEnabled)
-        //     {
-        //         MessageBox(NULL, L"auto aim Bot Enabled ", L"Info", MB_OK | MB_TOPMOST);
-        //     }
-        //     else
-        //     {
-        //         MessageBox(NULL, L"auto aim Bot Disabled", L"Info", MB_OK | MB_TOPMOST);
-        //     }
-
-        // }
     }
     renderer::destroy();
 
